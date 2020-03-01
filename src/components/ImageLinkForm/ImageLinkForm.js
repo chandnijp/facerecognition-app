be70f5bf4decb8a80cec4e4fc1e4f8ac13484799
Chandni Patel <chandnipatel10@hotmail.com>
2020-03-01 18:37:58 +0000

    changed button colour

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -11,7 +11,7 @@ const ImageLinkForm = ({ onInputChange, onImageSubmit }) => {
         <div className='form mv0 center pa4 br3 shadow-5'>
           <input className='f4 pa2 w-70 center' type='text' onChange={onInputChange} />
           <button 
-            className='w-30 grow f4 link ph3 pv2 dib white bg-light-blue' 
+            className='w-30 grow f4 link ph3 pv2 dib white bg-black' 
             onClick={onImageSubmit}
           >Detect</button>
         </div>
